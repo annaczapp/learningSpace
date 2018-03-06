88cfcb455b7be08b2ab6f21eb4712fa3045c7200
annaczapp <anna.czapp@neoworks.com>
2018-03-06 14:16:50 +0000

    new code

---
+++ src/com/company/Main.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import static com.company.Main.ReverseWords.reverseWords;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+       System.out.print(reverseWords("I like eating"));
+    }
+    public static class ReverseWords
+    {
+        public static String reverseWords(String str)
+        {
+            String[] words = str.split(" ");
+            String result = "";
+            for (int i = words.length-1; i >= 0; i--)
+            {
+                if (i == 0)
+                {
+                    result += words[i];
+                }
+                else
+                {
+                    result += words[i] + " ";
+                }
+            }
+            return result;
+        }
+    }
+}
